fee32223ed50a5aa3d7e581d2b661a5c50867856
Vilius Kraujutis <vilius@kraujutis.lt>
2012-12-06 01:17:31 +0200

    A little bit of UTF-8 encoding support
    in Sending screen

--- src/lt/andro/gcm4public/SendGCM2Clients.java
+++ src/lt/andro/gcm4public/SendGCM2Clients.java
@@ -1,3 +1,4 @@
+
 package lt.andro.gcm4public;
 
 import java.io.IOException;
@@ -21,57 +22,61 @@ import com.google.appengine.api.datastore.Query.FilterOperator;
 
 @SuppressWarnings("serial")
 public class SendGCM2Clients extends HttpServlet {
-	@SuppressWarnings("deprecation")
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
+    @Override
+    @SuppressWarnings("deprecation")
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
-		String senderId = req.getParameter("senderId");
-		String apiKey = req.getParameter("apiKey");
-		String title = req.getParameter("title");
-		String message = req.getParameter("message");
-		String url = req.getParameter("url");
-		if (senderId != null && !"".equalsIgnoreCase(senderId)) {
-			DatastoreService datastore = DatastoreServiceFactory
-					.getDatastoreService();
-			Query query = new Query("Client");
-			query = query.addFilter("senderId", FilterOperator.EQUAL, senderId);
-			PreparedQuery preparedQuery = datastore.prepare(query);
-			List<Entity> entities = preparedQuery.asList(FetchOptions.Builder
-					.withLimit(1000));
-			resp.setContentType("text/plain");
-			List<String> devices = new ArrayList<String>();
-			for (Entity entity : entities) {
-				devices.add((String) entity.getProperty("registrationId"));
-			}
-			MulticastResult result = sendMessageToDevice(apiKey, devices,
-					title, message, url);
+        String senderId = req.getParameter("senderId");
+        String apiKey = req.getParameter("apiKey");
+        String title = req.getParameter("title");
+        String message = req.getParameter("message");
+        String url = req.getParameter("url");
+        if (senderId != null && !"".equalsIgnoreCase(senderId)) {
+            DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+            Query query = new Query("Client");
+            query = query.addFilter("senderId", FilterOperator.EQUAL, senderId);
+            PreparedQuery preparedQuery = datastore.prepare(query);
+            List<Entity> entities = preparedQuery.asList(FetchOptions.Builder.withLimit(1000));
+            resp.setContentType("text/plain");
+            resp.setCharacterEncoding("UTF-8");
+            if (entities.size() != 0) {
+                List<String> devices = new ArrayList<String>();
+                for (Entity entity : entities) {
+                    devices.add((String)entity.getProperty("registrationId"));
+                }
+                MulticastResult result = sendMessageToDevice(apiKey, devices, title, message, url);
 
-			resp.getWriter().println("GCM Message fields:");
-			resp.getWriter().println("title: " + title);
-			resp.getWriter().println("message:" + message);
-			resp.getWriter().println("url: " + url);
-			resp.getWriter().println(
-					"Message sent to this number of devices:" + devices.size());
-			resp.getWriter().println("This API key was used: " + apiKey);
-			resp.getWriter().println("The result is: " + result);
-		} else {
-			resp.getWriter()
-					.println(
-							"Please provide your senderId in the URL parameter. Something like this: http://gcm4public.appspot.com/registeredgcmclients?senderId=716163315987");
-		}
-	}
+                resp.getWriter().println("GCM Message fields:");
+                resp.getWriter().println("title: " + title);
+                resp.getWriter().println("message:" + message);
+                resp.getWriter().println("url: " + url);
+                resp.getWriter()
+                        .println("Message sent to this number of devices:" + devices.size());
+                resp.getWriter().println("This API key was used: " + apiKey);
+                resp.getWriter().println("The result is: " + result);
+            } else {
+                resp.getWriter().println(
+                        "Are you sure, you provided correct sender Id? There is no devices registered for this senderId:"
+                                + senderId);
+            }
+        } else {
+            resp.getWriter()
+                    .println(
+                            "Please provide your senderId in the URL parameter. Something like this: http://gcm4public.appspot.com/registeredgcmclients?senderId=716163315987");
+        }
+    }
 
-	private MulticastResult sendMessageToDevice(String apiKey,
-			List<String> devices, String title, String message, String url) {
-		Sender sender = new Sender(apiKey);
-		Message gcmMessage = new Message.Builder().addData("title", title)
-				.addData("message", message).addData("url", url).build();
-		MulticastResult result = null;
-		try {
-			result = sender.send(gcmMessage, devices, 5);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return result;
-	}
+    private MulticastResult sendMessageToDevice(String apiKey, List<String> devices, String title,
+            String message, String url) {
+        Sender sender = new Sender(apiKey);
+        Message gcmMessage = new Message.Builder().addData("title", title)
+                .addData("message", message).addData("url", url).build();
+        MulticastResult result = null;
+        try {
+            result = sender.send(gcmMessage, devices, 5);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 }
